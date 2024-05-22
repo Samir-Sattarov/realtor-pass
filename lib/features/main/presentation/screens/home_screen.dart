3d683307e16d6d90404949799ea0d2115858006b
shakhawe <shakhaweb@gmail.com>
2024-05-22 20:57:55 +0500

    Added Home Screen

--- lib/features/main/presentation/screens/home_screen.dart
+++ lib/features/main/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +12,12 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+        ),
+      )),
     );
   }
 }
