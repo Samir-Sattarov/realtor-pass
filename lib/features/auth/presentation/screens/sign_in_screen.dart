48ccbd44b42d1ab7923a8f39a65bf5c6b40cd751
shakhawe <shakhaweb@gmail.com>
2024-05-22 20:49:44 +0500

    Fixed Sing In and Sign Up Screens

--- lib/features/auth/presentation/screens/sign_in_screen.dart
+++ lib/features/auth/presentation/screens/sign_in_screen.dart
@@ -1,14 +1,19 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:realtor_pass/app_core/app_core_library.dart';
 import 'package:realtor_pass/app_core/widgets/button_widget.dart';
+import 'package:realtor_pass/app_core/widgets/error_flash_bar.dart';
 import 'package:realtor_pass/app_core/widgets/text_form_field_widget.dart';
+import 'package:realtor_pass/features/auth/presentation/cubit/auth/auth_cubit.dart';
+import 'package:realtor_pass/features/auth/presentation/cubit/current_user/current_user_cubit.dart';
 import 'package:realtor_pass/features/auth/presentation/screens/confirm_otp_screen.dart';
-
+import 'package:realtor_pass/features/main/presentation/screens/main_screen.dart';
 import '../../../../resources/resources.dart';
 import 'forgot_password_screen.dart';
+import 'sign_up_screen.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -18,165 +23,221 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
-  TextEditingController controllerEmail = TextEditingController();
-  TextEditingController controllerPassword = TextEditingController();
+  final TextEditingController controllerEmail = TextEditingController();
+  final TextEditingController controllerPassword = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          physics: const ClampingScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 50.h,
-                ),
-                Text(
-                  "appName".tr(),
-                  style: TextStyle(
-                      color: AppStyle.darkGrey,
-                      fontSize: 20.sp,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w400),
-                ),
-                Text(
-                  "signIn".tr(),
-                  style: TextStyle(
-                      color: AppStyle.blue,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 32.sp),
-                ),
-                SizedBox(
-                  height: 58.h,
-                ),
-                TextFormFieldWidget(
-                  hintText: "name@email.com",
-                  controller: controllerEmail,
-                  title: "email".tr(),
-                ),
-                SizedBox(
-                  height: 30.h,
-                ),
-                TextFormFieldWidget(
-                  hintText: "12345",
-                  controller: controllerPassword,
-                  title: "password".tr(),
-                  isPassword: true,
-                ),
-                SizedBox(
-                  height: 46.h,
-                ),
-                ButtonWidget(
-                    title: "get".tr(),
-                    onTap: () {
-                      AnimatedNavigation.push(
-                          context: context,
-                          page: ConfirmOTPScreen(email: controllerEmail.text));
-                    }),
-                SizedBox(
-                  height: 14.h,
-                ),
-                Center(
-                  child: Text(
-                    "agree".tr(),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: AppStyle.darkGrey,
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ),
-                SizedBox(
-                  height: 2.h,
-                ),
-                GestureDetector(
-                  onTap: () => AnimatedNavigation.push(
-                    context: context,
-                    page: ForgotPasswordScreen(
-                      email: controllerEmail.text,
+      body: Form(
+        key: _formKey,
+        child: SafeArea(
+          child:BlocListener<AuthCubit, AuthState>(
+            listener: (context, state) {
+              if (state is AuthError) {
+                ErrorFlushBar(state.message).show(context);
+              }
+              if (state is AuthLoginSuccess) {
+                BlocProvider.of<CurrentUserCubit>(context).load();
+                AnimatedNavigation.push(context: context, page: const MainScreen());
+              }
+            },
+            child: SingleChildScrollView(
+              physics: const ClampingScrollPhysics(),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 50.h,
                     ),
-                  ),
-                  child: Center(
-                    child: RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: "forgotPassword".tr(),
-                            style: TextStyle(
-                                color: AppStyle.darkGrey,
-                                fontSize: 14.sp,
-                                fontWeight: FontWeight.w400),
+                    Text(
+                      "appName".tr(),
+                      style: TextStyle(
+                          color: AppStyle.darkGrey,
+                          fontSize: 20.sp,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      "signIn".tr(),
+                      style: TextStyle(
+                          color: AppStyle.blue,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 32.sp),
+                    ),
+                    SizedBox(
+                      height: 58.h,
+                    ),
+                    TextFormFieldWidget(
+                      hintText: "name@email.com",
+                      controller: controllerEmail,
+                      title: "email".tr(),
+                    ),
+                    SizedBox(
+                      height: 30.h,
+                    ),
+                    TextFormFieldWidget(
+                      hintText: "12345",
+                      controller: controllerPassword,
+                      title: "password".tr(),
+                      isPassword: true,
+                    ),
+                    SizedBox(
+                      height: 46.h,
+                    ),
+                    ButtonWidget(
+                        title: "get".tr(),
+                        onTap: () {
+                          if (_formKey.currentState!.validate()) {
+                            BlocProvider.of<AuthCubit>(context).signIn(
+                              email: controllerEmail.text,
+                              password: controllerPassword.text,
+                            );
+                          }
+
+                          AnimatedNavigation.push(
+                            context: context,
+                            page: ConfirmOTPScreen(
+                              email: controllerEmail.text,
+                            ),
+                          );
+                        }),
+                    SizedBox(
+                      height: 14.h,
+                    ),
+                    GestureDetector(
+                      onTap: () =>
+                          AnimatedNavigation.push(
+                            context: context,
+                            page: const SignUpScreen(),
                           ),
-                          const TextSpan(text: " "),
-                          TextSpan(
-                            text: "recover".tr(),
-                            style: TextStyle(
-                              color: AppStyle.blue,
-                              fontSize: 14.sp,
+                      child: Center(
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "dontHaveAnAccount".tr(),
+                                style: TextStyle(
+                                  color: AppStyle.dark,
+                                  fontSize: 14.sp,
+                                ),
+                              ),
+                              const TextSpan(text: ""),
+                              TextSpan(
+                                text: "signUp".tr(),
+                                style: TextStyle(
+                                  color: AppStyle.blue,
+                                  fontSize: 14.sp,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    GestureDetector(
+                      onTap: () =>
+                          AnimatedNavigation.push(
+                            context: context,
+                            page: ForgotPasswordScreen(
+                              email: controllerEmail.text,
                             ),
                           ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 16.h,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 1,
-                        decoration: BoxDecoration(
-                          color: const Color(0xffA1A1A1),
-                          borderRadius: BorderRadius.circular(100),
+                      child: Center(
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "forgotPassword".tr(),
+                                style: TextStyle(
+                                    color: AppStyle.darkGrey,
+                                    fontSize: 14.sp,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                              const TextSpan(text: " "),
+                              TextSpan(
+                                text: "recover".tr(),
+                                style: TextStyle(
+                                  color: AppStyle.blue,
+                                  fontSize: 14.sp,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                    SizedBox(width: 10.w),
-                    Text(
-                      "or".tr(),
-                      style: TextStyle(
-                        color: const Color(0xffA1A1A1),
-                        fontSize: 16.sp,
-                      ),
+                    SizedBox(
+                      height: 16.h,
                     ),
-                    SizedBox(width: 10.w),
-                    Expanded(
-                      child: Container(
-                        height: 1,
-                        decoration: BoxDecoration(
-                          color: const Color(0xffA1A1A1),
-                          borderRadius: BorderRadius.circular(100),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: 1,
+                            decoration: BoxDecoration(
+                              color: const Color(0xffA1A1A1),
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
                         ),
-                      ),
+                        SizedBox(width: 10.w),
+                        Text(
+                          "or".tr(),
+                          style: TextStyle(
+                            color: const Color(0xffA1A1A1),
+                            fontSize: 16.sp,
+                          ),
+                        ),
+                        SizedBox(width: 10.w),
+                        Expanded(
+                          child: Container(
+                            height: 1,
+                            decoration: BoxDecoration(
+                              color: const Color(0xffA1A1A1),
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
+                    SizedBox(
+                      height: 61.h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        _social(
+                          image: Svgs.tGoogle,
+                          onTap: () {},
+                        ),
+                        SizedBox(
+                          width: 14.w,
+                        ),
+                        _social(
+                          image: Svgs.tFacebook,
+                          onTap: () {},
+                        ),
+                        SizedBox(
+                          width: 14.w,
+                        ),
+                        _social(
+                          image: Svgs.tApple,
+                          onTap: () {},
+                        ),
+                      ],
+                    )
                   ],
                 ),
-                SizedBox(
-                  height: 61.h,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    _social(image: Svgs.tGoogle, onTap: () {}),
-                    SizedBox(
-                      width: 14.w,
-                    ),
-                    _social(image: Svgs.tFacebook, onTap: () {}),
-                    SizedBox(
-                      width: 14.w,
-                    ),
-                    _social(image: Svgs.tApple, onTap: () {}),
-                  ],
-                )
-              ],
+              ),
             ),
           ),
         ),
--- lib/features/auth/presentation/screens/sign_up_screen.dart
+++ lib/features/auth/presentation/screens/sign_up_screen.dart
@@ -1,9 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:realtor_pass/app_core/app_core_library.dart';
 import 'package:realtor_pass/app_core/widgets/button_widget.dart';
+import 'package:realtor_pass/app_core/widgets/error_flash_bar.dart';
 import 'package:realtor_pass/app_core/widgets/text_form_field_widget.dart';
+import 'package:realtor_pass/features/auth/presentation/cubit/registration/registration_cubit.dart';
+import 'package:realtor_pass/features/auth/presentation/screens/confirm_otp_screen.dart';
 import '../../../../app_core/utils/app_style.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -14,84 +19,113 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
-  TextEditingController controllerEmail = TextEditingController();
-  TextEditingController controllerPassword = TextEditingController();
-  TextEditingController controllerConfirmPassword = TextEditingController();
+  final TextEditingController controllerEmail = TextEditingController();
+  final TextEditingController controllerPassword = TextEditingController();
+  final TextEditingController controllerUsername = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 50.h,
-                ),
-                Text(
-                  "appName".tr(),
-                  style: TextStyle(
-                      color: AppStyle.darkGrey,
-                      fontSize: 20.sp,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w400),
-                ),
-                Text(
-                  "signUp".tr(),
-                  style: TextStyle(
-                      color: AppStyle.blue,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 32.sp),
-                ),
-                SizedBox(
-                  height: 58.h,
-                ),
-                TextFormFieldWidget(
-                  hintText: "name@email.com",
-                  controller: controllerEmail,
-                  title: "email".tr(),
-                  validator: FormValidator.validateEmail,
-                ),
-                SizedBox(
-                  height: 21.h,
-                ),
-                TextFormFieldWidget(
-                  hintText: "12345",
-                  controller: controllerPassword,
-                  title: "password".tr(),
-                  validator: FormValidator.password,
-                  isPassword: true,
-                ),
-                SizedBox(
-                  height: 21.h,
-                ),
-                TextFormFieldWidget(
-                  hintText: "12345",
-                  isPassword: true,
-                  controller: controllerConfirmPassword,
-                  title: "confirm".tr(),
-                  validator: (value) =>
-                      FormValidator.passwordConfirm(value, controllerPassword),
-                ),
-                SizedBox(
-                  height: 64.h,
-                ),
-                ButtonWidget(title: "Get a code", onTap: () {}),
-                SizedBox(height: 14.h,),
-                Center(
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    "byRegistering".tr(),
-                    style: TextStyle(
-                        color: AppStyle.blue,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14.sp),
+      body: Form(
+        key: _formKey,
+        child: SafeArea(
+          child: BlocListener<RegistrationCubit, RegistrationState>(
+            listener: (context, state) {
+              if (state is RegistrationError) {
+                ErrorFlushBar(state.message).show(context);
+              }
+              if (state is RegistrationSuccess) {
+                AnimatedNavigation.push(
+                  context: context,
+                  page: ConfirmOTPScreen(
+                    email: controllerEmail.text,
                   ),
-                )
-              ],
+                );
+              }
+            },
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 50.h,
+                    ),
+                    Text(
+                      "appName".tr(),
+                      style: TextStyle(
+                          color: AppStyle.darkGrey,
+                          fontSize: 20.sp,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      "signUp".tr(),
+                      style: TextStyle(
+                          color: AppStyle.blue,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 32.sp),
+                    ),
+                    SizedBox(
+                      height: 58.h,
+                    ),
+                    TextFormFieldWidget(
+                      hintText: "name@email.com",
+                      controller: controllerEmail,
+                      title: "email".tr(),
+                      validator: FormValidator.validateEmail,
+                    ),
+                    SizedBox(
+                      height: 21.h,
+                    ),
+                    TextFormFieldWidget(
+                      hintText: "name",
+                      controller: controllerUsername,
+                      title: "userName".tr(),
+                      validator: FormValidator.empty,
+                    ),
+                    SizedBox(
+                      height: 21.h,
+                    ),
+                    TextFormFieldWidget(
+                        hintText: "12345",
+                        isPassword: true,
+                        controller: controllerPassword,
+                        title: "password".tr(),
+                        validator: FormValidator.password),
+                    SizedBox(
+                      height: 64.h,
+                    ),
+                    ButtonWidget(
+                      title: "Get a code",
+                      onTap: () {
+                        if (_formKey.currentState!.validate()) {
+                          BlocProvider.of<RegistrationCubit>(context).signUp(
+                            email: controllerEmail.text,
+                            username: controllerUsername.text,
+                            password: controllerPassword.text,
+                          );
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 14.h,
+                    ),
+                    Center(
+                      child: Text(
+                        textAlign: TextAlign.center,
+                        "byRegistering".tr(),
+                        style: TextStyle(
+                            color: AppStyle.blue,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14.sp),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
