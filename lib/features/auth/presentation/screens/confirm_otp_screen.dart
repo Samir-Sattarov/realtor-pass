34d580f1c415da91056dd1cbfb2fdcb9b9e36741
shakhawe <shakhaweb@gmail.com>
2024-05-22 20:50:20 +0500

    Fixed Confirm Otp Screen

--- lib/features/auth/presentation/screens/confirm_otp_screen.dart
+++ lib/features/auth/presentation/screens/confirm_otp_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pinput/pinput.dart';
+import 'package:realtor_pass/app_core/widgets/error_flash_bar.dart';
 import 'package:realtor_pass/features/main/presentation/screens/main_screen.dart';
 import '../../../../app_core/app_core_library.dart';
 import '../../../../app_core/widgets/button_widget.dart';
@@ -29,149 +30,164 @@ class _ConfirmOTPScreenState extends State<ConfirmOTPScreen> {
     return KeyboardDismissOnTap(
       child: Scaffold(
         body: SafeArea(
-          child: SingleChildScrollView(
-            physics: const ClampingScrollPhysics(),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 55.h),
-                  Text(
-                    "appName".tr(),
-                    style: TextStyle(
-                      fontStyle: FontStyle.italic,
-                      color: AppStyle.dark,
-                      fontSize: 20.sp,
-                      height: 0,
-                      fontWeight: FontWeight.w400,
+          child: BlocListener<OtpCodeCubit, OtpCodeState>(
+            listener: (context, state) {
+              if(state is OtpCodeError){
+                ErrorFlushBar(state.message).show(context);
+              }
+              if (widget.onSuccess != null) {
+                widget.onSuccess?.call();
+              } else {
+                AnimatedNavigation.push(
+                  context: context,
+                  page: const MainScreen(),
+                );
+              }
+            },
+            child: SingleChildScrollView(
+              physics: const ClampingScrollPhysics(),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 55.h),
+                    Text(
+                      "appName".tr(),
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: AppStyle.dark,
+                        fontSize: 20.sp,
+                        height: 0,
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
-                  ),
-                  Text(
-                    "signUp".tr(),
-                    style: TextStyle(
-                      color: AppStyle.blue,
-                      fontSize: 32.sp,
-                      fontWeight: FontWeight.bold,
+                    Text(
+                      "signUp".tr(),
+                      style: TextStyle(
+                        color: AppStyle.blue,
+                        fontSize: 32.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 45.h),
-                  Text(
-                    "enterCode".tr(),
-                    style: TextStyle(
-                      color: AppStyle.blue,
-                      fontSize: 24.sp,
-                      fontWeight: FontWeight.w700,
+                    SizedBox(height: 45.h),
+                    Text(
+                      "enterCode".tr(),
+                      style: TextStyle(
+                        color: AppStyle.blue,
+                        fontSize: 24.sp,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 8.h),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "code".tr(),
+                    SizedBox(height: 8.h),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "code".tr(),
+                            style: TextStyle(
+                              color: AppStyle.dark,
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          const TextSpan(text: ""),
+                          TextSpan(
+                            text: widget.email,
+                            style: TextStyle(
+                              color: AppStyle.dark,
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 36.h),
+                    Center(
+                        child: SizedBox(
+                          height: 48.h,
+                          child: Pinput(
+                            length: 6,
+                            controller: controllerCode,
+                            defaultPinTheme: PinTheme(
+                              width: 48.w,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xffF4F4F4),
+                                  borderRadius: BorderRadius.circular(10.r),
+                                  border: Border.all(
+                                    color: const Color(0xffA1A1A1),
+                                    width: 1,
+                                  )),
+                            ),
+                          ),
+                        )),
+                    SizedBox(height: 10.h),
+                    Center(
+                      child: TextButton(
+                        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                        onPressed: () {},
+                        child: Text(
+                          "receive".tr(),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: AppStyle.grey,
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 161.h),
+                    Center(
+                      child: SizedBox(
+                        width: 248.w,
+                        child: Text(
+                          "in60secGetCode".tr(),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: AppStyle.grey,
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 32.h),
+                    ButtonWidget(
+                      title: "confirm",
+                      onTap: () {
+                        if (controllerCode.length == 6) {
+                          if (widget.onTapConfirm != null) {
+                            widget.onTapConfirm!.call(controllerCode.text);
+                          } else {
+                            BlocProvider.of<OtpCodeCubit>(context).confirm(
+                              code: controllerCode.text,
+                            );
+                          }
+                        }
+                        AnimatedNavigation.push(
+                            context: context, page: const MainScreen());
+                      },
+                    ),
+                    SizedBox(height: 8.h),
+                    Center(
+                      child: TextButton(
+                        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                        onPressed: () => Navigator.pop(context),
+                        child: Text(
+                          "goBack".tr(),
+                          textAlign: TextAlign.center,
                           style: TextStyle(
                             color: AppStyle.dark,
                             fontSize: 14.sp,
                             fontWeight: FontWeight.w400,
                           ),
                         ),
-                        const TextSpan(text: ""),
-                        TextSpan(
-                          text: widget.email,
-                          style: TextStyle(
-                            color: AppStyle.dark,
-                            fontSize: 16.sp,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 36.h),
-                  Center(
-                      child: SizedBox(
-                    height: 48.h,
-                    child: Pinput(
-                      length: 6,
-                      controller: controllerCode,
-                      defaultPinTheme: PinTheme(
-                        width: 48.w,
-                        decoration: BoxDecoration(
-                            color: const Color(0xffF4F4F4),
-                            borderRadius: BorderRadius.circular(10.r),
-                            border: Border.all(
-                              color: const Color(0xffA1A1A1),
-                              width: 1,
-                            )),
                       ),
                     ),
-                  )),
-                  SizedBox(height: 10.h),
-                  Center(
-                    child: TextButton(
-                      style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                      onPressed: () {},
-                      child: Text(
-                        "receive".tr(),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: AppStyle.grey,
-                          fontSize: 14.sp,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 161.h),
-                  Center(
-                    child: SizedBox(
-                      width: 248.w,
-                      child: Text(
-                        "in60secGetCode".tr(),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: AppStyle.grey,
-                          fontSize: 14.sp,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 32.h),
-                  ButtonWidget(
-                    title: "confirm",
-                    onTap: () {
-                      if (controllerCode.length == 6) {
-                        if (widget.onTapConfirm != null) {
-                          widget.onTapConfirm!.call(controllerCode.text);
-                        } else {
-                          BlocProvider.of<OtpCodeCubit>(context).confirm(
-                            code: controllerCode.text,
-                          );
-                        }
-                      }
-                      AnimatedNavigation.push(
-                          context: context, page: const MainScreen());
-                    },
-                  ),
-                  SizedBox(height: 8.h),
-                  Center(
-                    child: TextButton(
-                      style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                      onPressed: () => Navigator.pop(context),
-                      child: Text(
-                        "goBack".tr(),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: AppStyle.dark,
-                          fontSize: 14.sp,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
