3bf523b1a4c51b9076ed42f6ff3e03915db1f177
shakhawe <shakhaweb@gmail.com>
2024-05-22 20:46:33 +0500

    Added House Model and House Entity

---
+++ lib/features/main/core/entity/house_entity.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:equatable/equatable.dart';
+
+class HouseEntity extends Equatable {
+  final List<String> images;
+  final int id;
+  final String houseLocation;
+  final double lat;
+  final double lon;
+  final String houseType;
+  final int categoryId;
+  final String category;
+  final String description;
+  final int price;
+  final int square;
+  bool isFavorite;
+  final int bathroom;
+  final int rooms;
+
+  HouseEntity({
+    required this.id,
+    required this.houseLocation,
+    required this.isFavorite,
+    required this.houseType,
+    required this.category,
+    required this.categoryId,
+    required this.description,
+    required this.images,
+    required this.lon,
+    required this.lat,
+    required this.price,
+    required this.square,
+    required this.bathroom,
+    required this.rooms,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        houseLocation,
+        houseType,
+        category,
+        categoryId,
+        description,
+        images,
+        price,
+        square,
+        bathroom,
+        rooms,
+      ];
+}
---
+++ lib/features/main/core/models/house_model.dart
@@ -0,0 +1,82 @@
+import 'package:realtor_pass/features/main/core/entity/house_entity.dart';
+
+// ignore: must_be_immutable
+class HouseModel extends HouseEntity {
+  HouseModel(
+      {required super.id,
+      required super.houseLocation,
+      required super.isFavorite,
+      required super.houseType,
+      required super.category,
+      required super.categoryId,
+      required super.description,
+      required super.images,
+      required super.lat,
+      required super.lon,
+      required super.price,
+      required super.square,
+      required super.bathroom,
+      required super.rooms});
+
+  factory HouseModel.fromEntity(HouseEntity entity) {
+    return HouseModel(
+      id: entity.id,
+      houseLocation: entity.houseLocation,
+      isFavorite: entity.isFavorite,
+      houseType: entity.houseType,
+      category: entity.category,
+      categoryId: entity.categoryId,
+      description: entity.description,
+      images: List<String>.from(entity.images),
+      price: entity.price,
+      square: entity.square,
+      bathroom: entity.bathroom,
+      rooms: entity.rooms,
+      lat: entity.lat,
+      lon: entity.lon,
+    );
+  }
+  factory HouseModel.fromJson(Map<String, dynamic> json) {
+    return HouseModel(
+      id: json["id"],
+      houseLocation: json["houseLocation"],
+      isFavorite: json["isFavorite"],
+      houseType: json["houseType"],
+      category: json["category"],
+      categoryId: json["categoryId"],
+      description: json["description"],
+      images: List<String>.from(List<Map<String, dynamic>>.from(json['images'])
+          .map((Map<String, dynamic> e) => e["url"])
+          .toList()),
+      price: json["price"],
+      square: json["square"],
+      bathroom: json["bathroom"],
+      rooms: json["rooms"],
+      lon: json["lon"],
+      lat: json["lat"],
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "houseLocation": houseLocation,
+      "isFavorite": isFavorite,
+      "houseType": houseType,
+      "category": category,
+      "categoryId": categoryId,
+      "description": description,
+      "images": images.map((image) => {'url': image}).toList(),
+      "price": price,
+      "square": square,
+      "bathroom": bathroom,
+      "rooms": rooms,
+      "lat": lat,
+      "lon": lon
+    };
+  }
+
+  @override
+  String toString() {
+    return houseType;
+  }
+}
