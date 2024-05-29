feae25f94039b93605718c707235dbbca8d9ae0d
shakhawe <shakhaweb@gmail.com>
2024-05-29 19:48:00 +0500

    Connect Favorite Conditions Widget

---
+++ lib/features/main/presentation/widgets/fav_conditions_widget.dart
@@ -0,0 +1,102 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../app_core/app_core_library.dart';
+import '../../../../resources/resources.dart';
+
+class FavConditionsWidget extends StatelessWidget {
+  final Function() onTap;
+  const FavConditionsWidget({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 205,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12.r),
+          boxShadow: [
+            BoxShadow(
+              color: const Color(0xff001E21).withOpacity(0.2),
+              blurRadius: 21,
+            ),
+          ],
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Image.asset(
+                Images.tFavConditions,
+                width: 200.w,
+                height: 155.h,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.r),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ShaderMask(
+                    shaderCallback: (bounds) => const LinearGradient(
+                      colors: [AppStyle.blue, AppStyle.darkBlue],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ).createShader(bounds),
+                    child: Text(
+                      'favConditions'.tr(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 170.w,
+                    child: Text(
+                      "favorableConditionsText".tr(),
+                      style: TextStyle(
+                          color: const Color(0xff474747),
+                          fontSize: 10.sp,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 65.h,
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30.w, vertical: 5.h),
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [AppStyle.blue, AppStyle.darkBlue],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                      borderRadius: BorderRadius.circular(25.r),
+                    ),
+                    child: Text(
+                      "fillTheForm".tr(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10.sp,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
