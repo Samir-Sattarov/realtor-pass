12d473ec50d04972ce9da2eb7ad4707740006acb
shakhawe <shakhaweb@gmail.com>
2024-09-17 18:35:26 +0500

    Connect House Post Second Screen

---
+++ lib/features/main/presentation/screens/house_post_second_screen.dart
@@ -0,0 +1,219 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:realtor_pass/features/main/presentation/screens/post_house_stuff_screen.dart';
+import '../../../../app_core/app_core_library.dart';
+import '../../../../app_core/widgets/button_widget.dart';
+import '../../core/entity/house_post_entity.dart';
+
+class HousePostSecondScreen extends StatefulWidget {
+  final HousePostEntity entity;
+  const HousePostSecondScreen({super.key, required this.entity});
+
+  @override
+  HousePostSecondScreenState createState() => HousePostSecondScreenState();
+}
+
+class HousePostSecondScreenState extends State<HousePostSecondScreen> {
+   HousePostEntity postEntity = HousePostEntity.empty();
+
+
+  void incrementCounter(String type) {
+    setState(() {
+      switch (type) {
+        case 'guests':
+          postEntity = postEntity.copyWith(guests: postEntity.guests + 1);
+          break;
+        case 'bathrooms':
+          postEntity = postEntity.copyWith(bathrooms: postEntity.bathrooms + 1);
+          break;
+        case 'bedrooms':
+          postEntity = postEntity.copyWith(bedrooms: postEntity.bedrooms + 1);
+          break;
+        case 'beds':
+          postEntity = postEntity.copyWith(beds: postEntity.beds + 1);
+          break;
+      }
+    });
+  }
+
+  void decrementCounter(String type) {
+    setState(() {
+      switch (type) {
+        case 'guests':
+          if (postEntity.guests > 1) {
+            postEntity = postEntity.copyWith(guests: postEntity.guests - 1);
+          }
+          break;
+        case 'bathrooms':
+          if (postEntity.bathrooms > 1) {
+            postEntity = postEntity.copyWith(bathrooms: postEntity.bathrooms - 1);
+          }
+          break;
+        case 'bedrooms':
+          if (postEntity.bedrooms > 1) {
+            postEntity = postEntity.copyWith(bedrooms: postEntity.bedrooms - 1);
+          }
+          break;
+        case 'beds':
+          if (postEntity.beds > 1) {
+            postEntity = postEntity.copyWith(beds: postEntity.beds - 1);
+          }
+          break;
+      }
+    });
+  }
+
+  buildCounter(String label, int value, String type) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(label),
+        Row(
+          children: [
+            buildIconButton(Icons.remove, () => decrementCounter(type)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(value.toString()),
+            ),
+            buildIconButton(Icons.add, () => incrementCounter(type)),
+          ],
+        ),
+      ],
+    );
+  }
+
+  buildIconButton(IconData icon, VoidCallback onPressed) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: const Color(0xffDADADA), width: 1),
+        borderRadius: BorderRadius.circular(50.r),
+      ),
+      child: IconButton(
+        icon: Icon(icon),
+        onPressed: onPressed,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Text(
+                "share".tr(),
+                style: TextStyle(fontSize: 25.sp, fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 50.h,
+              ),
+              buildCounter('guests'.tr(), postEntity.guests, 'guests'),
+              SizedBox(
+                height: 10.h,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 1,
+                color: Color(0xffDADADA),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              buildCounter('bathrooms'.tr(), postEntity.bathrooms, 'bathrooms'),
+              SizedBox(
+                height: 10.h,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 1,
+                color: Color(0xffDADADA),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              buildCounter('bedrooms'.tr(), postEntity.bedrooms, 'bedrooms'),
+              SizedBox(
+                height: 10.h,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 1,
+                color: Color(0xffDADADA),
+              ),
+              SizedBox(height: 10.h,),
+              buildCounter('beds'.tr(), postEntity.beds, 'beds'),
+              SizedBox(
+                height: 10.h,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 1,
+                color: Color(0xffDADADA),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              SizedBox(height: 230.h,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.arrow_back_outlined)),
+                  SizedBox(
+                    width: 100.w,
+                    child: ButtonWidget(
+                      title: "next".tr(),
+                      onTap: () {
+                        bool hasMadeSelection = postEntity.guests > 1 ||
+                            postEntity.bathrooms > 1 ||
+                            postEntity.bedrooms > 1 ||
+                            postEntity.beds > 1;
+
+                        if (hasMadeSelection) {
+                          // Proceed to navigate
+                          AnimatedNavigation.push(
+                            context: context,
+                            page: const PostHouseStuffScreen(),
+                          );
+                        } else {
+                          // Show an alert dialog
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('Selection Required'),
+                                content: Text('Please select at least one option before proceeding.'),
+                                actions: [
+                                  TextButton(
+                                    child: Text('OK'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        }
+                      },
+
+                    ),
+                  ),
+                ],
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
