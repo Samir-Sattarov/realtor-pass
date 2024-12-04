feffc2966ce4d37336238d1b71a2aaa69fbeb3a9
shakhawe <shakhaweb@gmail.com>
2024-12-04 19:33:56 +0500

    Some changes in Listing Screen

--- lib/features/main/presentation/screens/listing_screen.dart
+++ lib/features/main/presentation/screens/listing_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:easy_localization/easy_localization.dart';
 import '../../../../app_core/app_core_library.dart';
 import '../../../../app_core/widgets/button_widget.dart';
 import '../../../../resources/resources.dart';
+import '../../../auth/presentation/cubit/session/session_cubit.dart';
+import '../../../auth/presentation/screens/sign_in_screen.dart';
 import '../../core/entity/house_post_entity.dart';
 import 'house_post_images_screen.dart';
-import 'house_post_screen.dart';
 
 class ListingScreen extends StatefulWidget {
   final HousePostEntity entity;
@@ -42,18 +44,9 @@ class _ListingScreenState extends State<ListingScreen> {
                   height: 200.h,
                 ),
                 SizedBox(height: 20.h),
-                Text(
-                  "You don't have any listings yet".tr(),
-                  style: TextStyle(
-                    fontSize: 18.sp,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
                 SizedBox(height: 10.h),
                 Text(
-                  "Create a listing with RealPass Setup and start getting booked."
-                      .tr(),
+                  "createHome".tr(),
                   style: TextStyle(
                     fontSize: 14.sp,
                     color: Colors.grey,
@@ -62,11 +55,24 @@ class _ListingScreenState extends State<ListingScreen> {
                 ),
                 SizedBox(height: 30.h),
                 Container(
-                    width: 300,
-                    child: ButtonWidget(
-                        title: "getStarted".tr(),
-                        onTap: () {
-                        }))
+                  width: 300.w,
+                  child: ButtonWidget(
+                    title: "getStarted".tr(),
+                    onTap: () {
+                      if (BlocProvider.of<SessionCubit>(context).state
+                          is SessionDisabled) {
+                        AnimatedNavigation.push(
+                            context: context, page: const SignInScreen());
+                      } else {
+                        AnimatedNavigation.push(
+                          context: context,
+                          page: HousePostImagesScreen(
+                              postEntity: housePostEntity),
+                        );
+                      }
+                    },
+                  ),
+                )
               ],
             ),
           ),
