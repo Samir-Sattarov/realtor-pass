b77b5227ced3a20487d43da191fcd820e7922bb5
shakhawe <shakhaweb@gmail.com>
2024-12-04 19:35:45 +0500

    Post House Screen was added

--- lib/features/main/presentation/screens/house_post_screen.dart
+++ lib/features/main/presentation/screens/house_post_screen.dart
@@ -13,14 +13,17 @@ import '../cubit/house_type/house_type_cubit.dart';
 import 'house_post_second_screen.dart';
 
 class HousePostScreen extends StatefulWidget {
-  const HousePostScreen({super.key});
+  final HousePostEntity entity;
+
+  const HousePostScreen(
+      {super.key, required this.entity});
 
   @override
   State<HousePostScreen> createState() => _HousePostScreenState();
 }
 
 class _HousePostScreenState extends State<HousePostScreen> {
-  late List<HouseTypeEntity>houseTypeEntity;
+  late List<HouseTypeEntity> houseTypeEntity;
   HouseTypeEntity? selectedCategory;
   HousePostEntity postEntity = HousePostEntity.empty();
 
@@ -28,6 +31,7 @@ class _HousePostScreenState extends State<HousePostScreen> {
   void initState() {
     super.initState();
     houseTypeEntity = [];
+    postEntity = widget.entity;
     initialize();
   }
 
@@ -36,21 +40,25 @@ class _HousePostScreenState extends State<HousePostScreen> {
       selectedCategory = selectedCategory == category ? null : category;
     });
   }
-  initialize() async{
+
+  initialize() async {
     await EasyLocalization.ensureInitialized().then((value) {
+      // ignore: use_build_context_synchronously
       BlocProvider.of<HouseTypeCubit>(context)
+          // ignore: use_build_context_synchronously
           .load(locale: context.locale.languageCode.toString());
     });
-
   }
 
   void onContinue() {
     if (selectedCategory != null) {
-      postEntity = postEntity.copyWith(category: selectedCategory!.title);
+      postEntity = postEntity.copyWith(category: selectedCategory!.id);
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => HousePostSecondScreen(entity: postEntity,),
+          builder: (context) => HousePostSecondScreen(
+            entity: postEntity,
+          ),
         ),
       );
     } else {
@@ -90,7 +98,7 @@ class _HousePostScreenState extends State<HousePostScreen> {
               if (mounted) {
                 Future.delayed(
                   Duration.zero,
-                      () {
+                  () {
                     setState(() {
                       houseTypeEntity = state.results.housesType;
                     });
@@ -117,15 +125,12 @@ class _HousePostScreenState extends State<HousePostScreen> {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: SizedBox(
-                      height: MediaQuery
-                          .of(context)
-                          .size
-                          .height * 0.6,
+                      height: MediaQuery.of(context).size.height * 0.6,
                       child: BlocBuilder<HouseTypeCubit, HouseTypeState>(
                         builder: (context, state) {
                           return GridView.builder(
                             gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                const SliverGridDelegateWithFixedCrossAxisCount(
                               crossAxisCount: 2,
                               childAspectRatio: 3 / 2.4,
                               crossAxisSpacing: 10,
@@ -143,8 +148,7 @@ class _HousePostScreenState extends State<HousePostScreen> {
                                   onTap: () => onCategoryTap(category),
                                   child: Ink(
                                     decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(
-                                          10.r),
+                                      borderRadius: BorderRadius.circular(10.r),
                                       border: Border.all(
                                           color: isSelected
                                               ? Colors.transparent
@@ -160,9 +164,17 @@ class _HousePostScreenState extends State<HousePostScreen> {
                                             horizontal: 20),
                                         child: Column(
                                           mainAxisAlignment:
-                                          MainAxisAlignment.center,
+                                              MainAxisAlignment.center,
                                           children: [
-                                            SizedBox(child: Center(child: Image.network(category.image, fit: BoxFit.contain,)), height: 64.r, width: 64.r,),
+                                            SizedBox(
+                                              child: Center(
+                                                  child: Image.network(
+                                                category.image,
+                                                fit: BoxFit.contain,
+                                              )),
+                                              height: 64.r,
+                                              width: 64.r,
+                                            ),
                                             SizedBox(height: 10.h),
                                             FittedBox(
                                               child: Text(
