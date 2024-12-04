d23c2757db9ac5622bc3c63439b23d46d276338b
shakhawe <shakhaweb@gmail.com>
2024-12-04 19:37:10 +0500

    Connected My Houses Screen

---
+++ lib/features/main/presentation/screens/my_houses_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyHousesScreen extends StatefulWidget {
+  const MyHousesScreen({super.key});
+
+  @override
+  State<MyHousesScreen> createState() => _MyHousesScreenState();
+}
+
+class _MyHousesScreenState extends State<MyHousesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
