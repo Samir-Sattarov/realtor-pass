a3b5ee905c021770abf414ec0aae131130d56ffb
shakhawe <shakhaweb@gmail.com>
2024-12-18 18:44:47 +0500

    some changes

--- lib/features/main/presentation/cubit/favorite/favorite_cubit.dart
+++ lib/features/main/presentation/cubit/favorite/favorite_cubit.dart
@@ -7,16 +7,15 @@ import 'favorite_state.dart';
 
 class FavoriteHousesCubit extends Cubit<FavoriteHousesState> {
   final StorageService storage;
+  final String userId; // Допустим, вы передаете userId в кубит при его создании
 
-  FavoriteHousesCubit(this.storage) : super(const FavoriteHousesInitial()) {
+  FavoriteHousesCubit(this.storage, {required this.userId}) : super(const FavoriteHousesInitial()) {
     load();
   }
 
-  static List<HouseEntity> _listFavoriteHouses = [];
+  List<HouseEntity> _listFavoriteHouses = [];
 
-  List<HouseEntity> get listFavoriteHouses => _listFavoriteHouses;
-
-  static const String _favoriteHousesKey = 'favorite_houses';
+  String get _favoriteHousesKey => 'favorite_houses_$userId';
 
   Future<void> load() async {
     emit(const FavoriteHousesLoading());
@@ -25,8 +24,7 @@ class FavoriteHousesCubit extends Cubit<FavoriteHousesState> {
     final listFavHousesDataJson =
         await storage.get(key: _favoriteHousesKey) ?? [];
 
-    _listFavoriteHouses = List.from(listFavHousesDataJson)
-        .map((e) {
+    _listFavoriteHouses = List.from(listFavHousesDataJson).map((e) {
       try {
         final Map<String, dynamic> data = Map<String, dynamic>.from(e);
         return HouseMapper.fromDTO(HouseDTO.fromJson(data));
@@ -34,9 +32,7 @@ class FavoriteHousesCubit extends Cubit<FavoriteHousesState> {
         print("Error processing element: $error");
         return null;
       }
-    })
-        .whereType<HouseEntity>()
-        .toList();
+    }).whereType<HouseEntity>().toList();
 
     emit(FavoriteHousesLoaded(_listFavoriteHouses));
   }
@@ -44,28 +40,14 @@ class FavoriteHousesCubit extends Cubit<FavoriteHousesState> {
   Future<void> addFavorite(HouseEntity house) async {
     if (!_listFavoriteHouses.any((item) => item.id == house.id)) {
       _listFavoriteHouses.add(house);
-
-      await storage.save(
-        key: _favoriteHousesKey,
-        value: _listFavoriteHouses
-            .map((e) => HouseMapper.toDTO(e).toJson())
-            .toList(),
-      );
-
+      await _saveToStorage();
       emit(FavoriteHousesChanged(_listFavoriteHouses));
     }
   }
 
   Future<void> removeFavorite(int houseId) async {
     _listFavoriteHouses.removeWhere((item) => item.id == houseId);
-
-    await storage.save(
-      key: _favoriteHousesKey,
-      value: _listFavoriteHouses
-          .map((e) => HouseMapper.toDTO(e).toJson())
-          .toList(),
-    );
-
+    await _saveToStorage();
     emit(FavoriteHousesChanged(_listFavoriteHouses));
   }
 
@@ -75,9 +57,14 @@ class FavoriteHousesCubit extends Cubit<FavoriteHousesState> {
 
   Future<void> deleteAllFavorites() async {
     _listFavoriteHouses.clear();
-
     await storage.delete(key: _favoriteHousesKey);
-
     emit(FavoriteHousesChanged(_listFavoriteHouses));
+  }
+
+  Future<void> _saveToStorage() async {
+    await storage.save(
+      key: _favoriteHousesKey,
+      value: _listFavoriteHouses.map((e) => HouseMapper.toDTO(e).toJson()).toList(),
+    );
   }
 }
